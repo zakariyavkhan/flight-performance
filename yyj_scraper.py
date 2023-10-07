828709bda68e0995f1f44d71b5490a961cd215a3
Zakariya Khan <zakariyavkhan@gmail.com>
2023-10-06 18:14:13 -0700

    update parsing delayed flights

--- yyj_scraper.py
+++ yyj_scraper.py
@@ -26,18 +26,20 @@ def get_client():
     return client
 
 
-def get_flights(url):
+def get_flights(url, table_id):
     """
     Gets the table of flights from the given url
     :param url: the url to scrape
+    :param table_id: the id of the table to scrape
     :return: the table of flights
     """
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
 
     # get the table with id "flightsToday", select rows with class "arrival" or "departure"
-    table = soup.find("table", {"id": "flightsToday"})
-    table = table.find_all("tr", class_=["arrival", "departure"])
+    table = soup.find("table", {"id": table_id})
+    if table:
+        table = table.find_all("tr", class_=["arrival", "departure"])
 
     return table
 
@@ -45,6 +47,7 @@ def get_flights(url):
 def parse_flights(table):
     """
     Parses the table of flights
+    :caveat: this function breaks on every Jan 1st for delayed flights
     :param table: the table of flights
     :return: list flights
     """
@@ -90,12 +93,34 @@ def parse_flights(table):
 
 
 def main():
-    flight_table = get_flights(config["URL"])
+    delayed_flights = get_flights(config["URL"], "flightsYesterday")
+    flight_table = get_flights(config["URL"], "flightsToday")
     flights = parse_flights(flight_table)
     client = get_client()
     db = client[config["DB_NAME"]]
     flights_collection = db[config["COLLECTION"]]
-    
+
+    update_str = ""
+    if delayed_flights:
+        delayed_flights = parse_flights(delayed_flights)
+        # for each entry in delayed flights find the corresponding flight
+        # in the flights collection by scheduled_timestamp and flight_num
+        # and update the actual_timestamp
+        updated = 0
+        for delayed_flight in delayed_flights:
+            query = {
+                "scheduled_timestamp": delayed_flight["scheduled_timestamp"],
+                "flight_num": delayed_flight["flight_num"],
+            }
+            update = {"$set": {"actual_timestamp": delayed_flight["actual_timestamp"]}}
+            try:
+                result = flights_collection.update_one(query, update)
+                updated += result.modified_count
+            except Exception as e:
+                print("Error updating document in MongoDB: ", e)
+                exit(1)
+        update_str = f"Updated {updated} documents\n"
+
     try:
         results = flights_collection.insert_many(flights)
     except Exception as e:
@@ -105,7 +130,12 @@ def main():
     # write to log file with timestamp
     with open("flights.log", "a") as f:
         f.write(
-            f"{datetime.now().strftime('%Y-%m-%d %H:%M')}: Inserted {len(results.inserted_ids)} documents into {config['COLLECTION']}\n"
+            f"""
+              {datetime.now().strftime('%Y-%m-%d %H:%M')}: 
+              Inserted {len(results.inserted_ids)} documents 
+              into {config['COLLECTION']}\n
+              """
+            + update_str
         )
     client.close()
 
